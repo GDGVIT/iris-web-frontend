5a06baee761b4812d58037ce23e9bb65b4497f67
N0x_c0dx <Nirmitjatana@users.noreply.github.com>
2020-06-19 20:39:55 +0530

    Extra files removed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import {landingContent} from "./components/landingContent";
+import App from "./components/landingContent";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <App/>
+      // <landingContent/>
+      ,
   document.getElementById('root')
 );
 
