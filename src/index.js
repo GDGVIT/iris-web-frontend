75fa0246fe52d8ca80a6339585236b50c1abb3a6
N0x_c0dx <Nirmitjatana@users.noreply.github.com>
2020-06-21 15:46:15 +0530

    dark theme palatte created

--- src/index.js
+++ src/index.js
@@ -2,11 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { green , grey } from '@material-ui/core/colors';  
 
 // import {landingContent} from "./components/landingContent";
-import App from "./components/landingContent";
+// import App from "./components/landingContent";
+import CenteredTabs from "./components/tabs";
+
+const theme = createMuiTheme({
+  
+  palette: {
+    primary: {
+      main: green["A400"],
+      light: green[400],
+      dark :green[900],
+    },
+    secondary: {
+      main: grey[800],
+    },
+    background: {
+      default: grey[800],
+      paper:grey["#303030"]
+    },
+    text:{
+      primary: grey[50]
+    },
+
+  },
+  
+});
+
+console.log(theme)
 ReactDOM.render(
-      <App/>
+  <ThemeProvider theme={theme}>
+      <CenteredTabs/>
+      {/* <App/> */}
+      </ThemeProvider>
+      // <Tab/>
       // <landingContent/>
       ,
   document.getElementById('root')
