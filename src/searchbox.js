bb400f52a21a74d407977b026724497cd6e35d3a
N0x_c0dx <Nirmitjatana@users.noreply.github.com>
2020-06-25 20:57:18 +0530

    basic search box componenet

---
+++ src/searchbox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class searchbox extends React.Component {
+    render(){
+        return(
+            <div className="search-box">
+                Nirmit jatana
+            </div>
+        )
+    }
+}
