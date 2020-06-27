e2d9d7842090f482217b2e4f53be73ad29fa7857
N0x_c0dx <Nirmitjatana@users.noreply.github.com>
2020-06-27 14:46:13 +0530

    search box component added

--- src/searchbox.js
+++ src/searchbox.js
@@ -3,7 +3,13 @@ export default class searchbox extends React.Component {
     render(){
         return(
             <div className="search-box">
-                Nirmit jatana
+                <form>
+                <label>
+                    Name:<br/>
+                    <input type="text" name="name" />
+                </label><br/>
+                <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
