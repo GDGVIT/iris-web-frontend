1029068bb2871f912e99bc3c2e7e01473cb7769c
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-08-20 10:30:17 +0530

    Create download.js

---
+++ src/download.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class download extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default download;
