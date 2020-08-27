52736cb7c54ae6784ff798e743e87ca581ea8b1f
Nirmitjatana <52108077+Nirmitjatana@users.noreply.github.com>
2020-08-27 22:18:39 +0530

    Content

--- src/download.js
+++ src/download.js
@@ -11,8 +11,8 @@ class download extends React.Component{
         return(
             <div className="down">
                 <h1 style={{color:"#22d46c"}}>Coming Sooooon!</h1>
-                <h3>GUI is in development stage. If you wanna get it first, contact us.</h3>
-                <h4>Downloadable GUI provides additional functionalities such as deeper searches and search filters.</h4>
+                <h3>we limit our searches to a specific depth to ensure performance but GUI will use your underlying hardware so will be faster. <br/>Downloadable GUI provides additional functionalities such as deeper searches and search filters.</h3>
+                <h3>GUI is in development stage. If you wanna get it first, contact us!</h3>
             </div>
         )
     }
